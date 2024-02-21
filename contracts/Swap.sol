10e0b10e89dfb0319cb9649a52ecbc0e7dc65c0e
PhantomOz <faniogor@gmail.com>
2024-02-21 16:32:30 +0100

    This commit will add swapB function

--- contracts/Swap.sol
+++ contracts/Swap.sol
@@ -2,6 +2,8 @@
 pragma solidity ^0.8.18;
 import {IERC20} from "./IERC20.sol";
 
+error INSUFFICIENT_FUNDS();
+
 contract Swap {
     IERC20 tokenA;
     IERC20 tokenB;
@@ -13,9 +15,17 @@ contract Swap {
 
     function swapA(uint256 _amount) external {
         if (tokenA.balanceOf(msg.sender) >= _amount) {
-            revert();
+            revert INSUFFICIENT_FUNDS();
         }
         tokenA.transferFrom(msg.sender, address(this), _amount);
         tokenB.transfer(msg.sender, _amount);
+    }
+
+    function swapB(uint256 _amount) external {
+        if (tokenB.balanceOf(msg.sender) >= _amount) {
+            revert INSUFFICIENT_FUNDS();
+        }
+        tokenB.transferFrom(msg.sender, address(this), _amount);
+        tokenA.transfer(msg.sender, _amount);
     }
 }
